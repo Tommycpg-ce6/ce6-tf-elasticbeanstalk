7bdcff2872aae7f96d245ea8f912c2208cb2c66f
Tommycpg-ce6 <tommy.chuapg@gmail.com>
2024-07-09 21:50:09 +0800

    update

--- output.tf
+++ output.tf
@@ -1,8 +1,8 @@
-output "app_name" {
+output "tom-eb-app" {
   value = aws_elastic_beanstalk_application.eb_app.name
 }
 
-output "env_name" {
+output "tom-eb-app-env" {
   value = aws_elastic_beanstalk_environment.eb_env.name
 }
 
